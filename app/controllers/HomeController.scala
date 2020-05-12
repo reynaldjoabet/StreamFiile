bd6c197f22e68888a5116662a938c993dbb414f5
reynaldjoabet <reynaldjoabetgmail.com>
2020-05-13 01:47:41 +0400

    A simple app to stream files

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,36 @@
+package controllers
+import akka.stream.scaladsl.Source
+import javax.inject._
+import play.api.mvc._
+
+import scala.concurrent.ExecutionContext
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+  /*implicit def wString(implicit codec: Codec): Writeable[String] =
+    Writeable[String](str => codec.encode(str))
+
+   */
+  implicit  val ec:ExecutionContext=cc.executionContext
+  def index: Action[AnyContent] = Action {
+    Ok(views.html.index("Your new application is ready."))
+  }
+// class Writeable[-A](val transform: A => ByteString, val contentType: Option[String])
+  // Writeable Transform a value of type A to a Byte Array.
+  // simply import the default implementation
+  def streamList: Action[AnyContent] =Action{ request=>
+    import  play.api.http.Writeable.wString
+    val source=Source.apply(List("Peter","Kelly","Pius","Herman","Paul"))
+    Ok.chunked(source)
+  }
+  def streamFile: Action[AnyContent] =Action{ request=>
+    val file= new java.io.File("location of file")
+    Ok.sendFile(file)
+  }
+
+}
+
---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "StreamingFile"
+ 
+version := "1.0" 
+      
+lazy val `streamingfile` = (project in file(".")).enablePlugins(PlayScala)
+
+resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
+      
+resolvers += "Akka Snapshot Repository" at "https://repo.akka.io/snapshots/"
+      
+scalaVersion := "2.12.10"
+
+libraryDependencies ++= Seq( ws  , guice )
+
+      
